f38e6a1593f799b39c8a7e27a17c4ed54da5ae75
shayshab <shayshabazad@gmail.com>
2018-09-29 11:17:36 +0600

    1st

---
+++ app/src/main/java/com/example/shays/facebookhack/Phising.java
@@ -0,0 +1,14 @@
+package com.example.shays.facebookhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Phising extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phising);
+        setTitle("Mobile Phone Hack");
+    }
+}
---
+++ app/src/main/java/com/example/shays/facebookhack/Spassword.java
@@ -0,0 +1,14 @@
+package com.example.shays.facebookhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spassword);
+        setTitle("Saved Password");
+    }
+}
